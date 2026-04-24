912484aa1784fccc7212c26b440b637f9d6dc414
Timohankp <timohankp@gmail.com>
2026-04-24 08:50:03 +0300

    створив клас людини

---
+++ Завдання 4.py
@@ -0,0 +1,9 @@
+class People:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def show_info(self):
+        print(f"ім'я: {self.name}, вік: {self.age}")
+
+
